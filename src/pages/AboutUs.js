2b0536328744cd74bd7f25b065f9136f73c80053
Sahibjot01 <sahibjot70@gmail.com>
2024-05-16 22:01:48 -0400

    3. Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AboutSection from "../Components/AboutSection";
+import ServicesSection from "../Components/ServicesSection";
 
 const AboutUs = () => {
   return (
-    <div>
+    <>
       <AboutSection />
-    </div>
+      <ServicesSection />
+    </>
   );
 };
 
