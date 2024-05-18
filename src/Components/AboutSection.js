d05b335ef283ee1555655fd26ba445f418efcc82
Sahibjot01 <sahibjot70@gmail.com>
2024-05-17 22:52:57 -0400

    5. Used Styled Component to style global style and about section

--- src/Components/AboutSection.js
+++ src/Components/AboutSection.js
@@ -1,34 +1,65 @@
 import React from "react";
 import home1 from "../img/home1.png";
+//styled component
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <StyledAboutDiv>
+      <StyledDescriptionDiv>
         <div className="title">
-          <div className="hide">
+          <StyledHideDiv>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </StyledHideDiv>
+          <StyledHideDiv>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </StyledHideDiv>
+          <StyledHideDiv>
             <h2>true.</h2>
-          </div>
+          </StyledHideDiv>
         </div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className="image">
+      </StyledDescriptionDiv>
+      <StyledImageDiv>
         <img src={home1} alt="guy with a professional camea" />
-      </div>
-    </div>
+      </StyledImageDiv>
+    </StyledAboutDiv>
   );
 };
 
+const StyledAboutDiv = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: #f5f5f5;
+`;
+
+const StyledDescriptionDiv = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const StyledImageDiv = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+const StyledHideDiv = styled.div`
+  overflow-y: hidden;
+`;
 export default AboutSection;
