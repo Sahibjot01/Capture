69c1039f25dd4b71f2b11e2b52e8d0cc4a4b4132
Sahibjot01 <sahibjot70@gmail.com>
2024-05-21 03:52:08 -0400

    13. Added animation on About section.

--- src/Components/AboutSection.js
+++ src/Components/AboutSection.js
@@ -10,6 +10,7 @@ import {
 import styled from "styled-components";
 //import framer motion
 import { motion } from "framer-motion";
+import { titleAnimation, fadeAnimation, photoAnimation } from "../animation";
 
 const AboutSection = () => {
   return (
@@ -17,25 +18,29 @@ const AboutSection = () => {
       <StyledDescriptionDiv>
         <motion.div>
           <StyledHideDiv>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnimation}>We work to make</motion.h2>
           </StyledHideDiv>
           <StyledHideDiv>
-            <motion.h2>
+            <motion.h2 variants={titleAnimation}>
               your <span>dreams</span> come
             </motion.h2>
           </StyledHideDiv>
           <StyledHideDiv>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnimation}>true.</motion.h2>
           </StyledHideDiv>
         </motion.div>
-        <p>
+        <motion.p variants={fadeAnimation}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fadeAnimation}>Contact Us</motion.button>
       </StyledDescriptionDiv>
       <StyledImageDiv>
-        <img src={home1} alt="guy with a professional camea" />
+        <motion.img
+          variants={photoAnimation}
+          src={home1}
+          alt="guy with a professional camea"
+        />
       </StyledImageDiv>
     </StyledAboutDiv>
   );
--- src/animation.js
+++ src/animation.js
@@ -6,13 +6,57 @@ export const pageAnimation = {
   show: {
     opacity: 1,
     y: 0,
-    transition: { duration: 1 },
+    transition: {
+      duration: 1,
+      staggerChildren: 0.25,
+    },
   },
   exit: {
     opacity: 0,
     y: 300,
     transition: {
-      duration: 1,
+      duration: 0.5,
+    },
+  },
+};
+
+export const titleAnimation = {
+  hidden: {
+    y: 200,
+  },
+  show: {
+    y: 0,
+    transition: {
+      duration: 0.75,
+      ease: "easeOut",
+    },
+  },
+};
+
+export const fadeAnimation = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 0.75,
+      ease: "easeOut",
+    },
+  },
+};
+
+export const photoAnimation = {
+  hidden: {
+    opacity: 0,
+    scale: 1.5,
+  },
+  show: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.75,
+      ease: "easeIn",
     },
   },
 };
